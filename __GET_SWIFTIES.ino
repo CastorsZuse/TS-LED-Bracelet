08f8af052669aae0d24284a0817674995fb80efa
Nolan Cash <76930360+CastorsZuse@users.noreply.github.com>
2024-11-13 11:37:25 -0500

    Bracelet

--- __GET_SWIFTIES.ino
+++ __GET_SWIFTIES.ino
@@ -1,3 +1,5 @@
+
+
 // iR receiver pin
 const int irPin = 10;
 
@@ -5,7 +7,8 @@ const int irPin = 10;
 const int LEDred = 3;
 const int LEDgreen = 5;
 const int LEDblue = 6;
-const int NEOIO = A4;
+
+
 
 // Veritables for color set
 enum Color : int {
@@ -14,6 +17,9 @@ enum Color : int {
   BLUE  = 4
 };
 
+
+
+
 //Variables for IR Receiver
 #define DECODE_NEC
 #include <IRremote.h>
@@ -21,35 +27,17 @@ unsigned long current_code = 0;
 
 
 
-//Control IR numbers 3.0 UPDATE
-//const long iRIN_botACTIVATION = 3409232445;
-//const long iRIN_botSTOP_R =     3409195215;
-//const long iRIN_botSTOP_G =     3409214085;
-//const long iRIN_botSTOP_B =     3409189095;
-//const long iRIN_botSTOP_RB =    3409187055;
-//const long iRIN_botSTOP_RG =    3409197255;
-//const long iRIN_botSTOP_BG =    3409205925;
-//const long iRIN_botBumpLEFT =   3409224285;
-//const long iRIN_botSTALL =      3409207965;
-//const long iRIN_botBumpRIGHT =  3409240605;
-//const long iRIN_botNOPEleft =   3409240095;
-//const long iRIN_botNOPEback  =  3409225815;
-//const long iRIN_botNOPEright =  3409219695;
+////Control IR numbers TEST REMOTE SILVER
+const long iRIN_botACTIVATION = 3409232445;
+const long iRIN_botSTOP_R =     3409224285;
+const long iRIN_botSTOP_G =     3409207965;
+const long iRIN_botSTOP_B =     3409240605;
+const long iRIN_botSTOP_RB =    3409240095;
+const long iRIN_botSTOP_RG =    3409225815;
+const long iRIN_botSTOP_BG =    3409219695;
 
-//Control IR numbers TEST REMOTE
-const long iRIN_botACTIVATION = 3158572800;
-const long iRIN_botSTOP_R =     4077715200;
-const long iRIN_botSTOP_G =     3877175040;
-const long iRIN_botSTOP_B =     2707357440;
-const long iRIN_botSTOP_RB =    4144561920;
-const long iRIN_botSTOP_RG =    3810328320;
-const long iRIN_botSTOP_BG =    2774204160;
-const long iRIN_botBumpLEFT =   3125149440;
-const long iRIN_botSTALL =      3108437760;
-const long iRIN_botBumpRIGHT =  3091726080;
-const long iRIN_botNOPEleft =   4161273600;
-const long iRIN_botNOPEback  =  3927310080;
-const long iRIN_botNOPEright =  4127850240;
+
+
 
 
 // Veritables for setting leds 
@@ -57,25 +45,38 @@ void setLEDs(int colorValue, bool neoValue) {
   digitalWrite(LEDblue, BLUE & colorValue);
   digitalWrite(LEDgreen, GREEN & colorValue);
   digitalWrite(LEDred, RED & colorValue);
-
 }
 
-void SetLEDs(int colorValue) {
+void stopAndSetLEDs(int colorValue) {
   setLEDs(colorValue, false);
 }
 
 
+
+
 void setup() {
+////////////////////////////////////////////////////
+/////////////////////////// DEBUG NOTE /////////////
+//         for debugging led feeback, comment out...
+
+  IrReceiver.begin(irPin, ENABLE_LED_FEEDBACK);
+  IrReceiver.begin(irPin); // <-- THIS LINE
+
+// WARNING! ledRED WILL NOT WORK IN DEBUG MODE // 
+///////////////////////////////////////////////////  
+
 
   pinMode(LEDred, OUTPUT);
   pinMode(LEDgreen, OUTPUT);
   pinMode(LEDblue, OUTPUT);
-  pinMode(NEOIO, OUTPUT);
   IrReceiver.enableIRIn();
   Serial.begin(9600);
 }
 
 void loop() {
+
+
+
 
   if (IrReceiver.decode()){
     current_code = IrReceiver.decodedIRData.decodedRawData;
@@ -83,44 +84,57 @@ void loop() {
     Serial.println(current_code);
     IrReceiver.resume();        
 
+
     switch (current_code) { 
-      
 
-      case iRIN_botSTOP_R:
-      SetLEDs(RED);
-        break;
-        
-      case iRIN_botSTOP_G:
-      SetLEDs(GREEN);
-        break;
-        
-      case iRIN_botSTOP_B:
-       SetLEDs(BLUE);
-        break;
-        
-      case iRIN_botSTOP_RB:
-       SetLEDs(RED | BLUE);
-        break;
-        
-      case iRIN_botSTOP_RG:
-       SetLEDs(RED | GREEN);
-        break;
-        
-      case iRIN_botSTOP_BG:
-       SetLEDs(BLUE | GREEN);
+      case iRIN_botACTIVATION:
+        Serial.println("BOT ACTIVATION");
+        stopAndSetLEDs(RED | BLUE | GREEN);
         break;
 
-      case iRIN_botBumpLEFT:
-       SetLEDs(RED | BLUE | GREEN);
-
-       break;
+      case iRIN_botSTOP_R:{
+        Serial.println("botSTOP_RED");
+        stopAndSetLEDs(RED);
+        delay(50);
+       }
+        break;
         
-     
+      case iRIN_botSTOP_G:{
+        Serial.println("botSTOP_GREEN");
+        stopAndSetLEDs(GREEN);
+       }
+        break;
+        
+      case iRIN_botSTOP_B:{
+        Serial.println("botSTOP_BLUE");
+        stopAndSetLEDs(BLUE);
+       }
+        break;
+        
+      case iRIN_botSTOP_RB:{
+        Serial.println("botSTOP_RED_BLUE");
+        stopAndSetLEDs(RED | BLUE);
+       }
+        break;
+        
+      case iRIN_botSTOP_RG:{
+        Serial.println("botSTOP_RED_GREEN");
+        stopAndSetLEDs(RED | GREEN);
+       }
+        break;
+        
+      case iRIN_botSTOP_BG:{
+        Serial.println("botSTOP_BLUE_GREEN");
+        stopAndSetLEDs(BLUE | GREEN);
+       }
+        break;
+
+
+        
+
+
       default:
         break;
     }
   }
-}
-
-
- 
+  }
